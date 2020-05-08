ab872973a50256f99de28d73718e127787190815
ailanthustng <ailanthustng@gmail.com>
2020-05-08 16:41:16 +0800

    Added OverallTable, TableEntry, RequestButton, RequestModal, RequestForm

--- front-end/src/RequestForm.js
+++ front-end/src/RequestForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import Form from "react-bootstrap/Form";
 
@@ -6,25 +7,34 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 function RequestForm(props) {
-    return(
-      <Form>
-          <Form.Group controlId="Application">
-              <Form.Label>Application Name</Form.Label>
-              <Form.Control as="select">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-              </Form.Control>
-              <Form.Text>Choose an application</Form.Text>
-          </Form.Group>
-          <Modal.Footer>
-            <Button>Submit</Button>
-            <Button onClick={props.onClick}>Close</Button>
-          </Modal.Footer>
-      </Form>
+
+    return (
+        <Form>
+            <Form.Group controlId="Application">
+                <Form.Label>Application Name</Form.Label>
+                <Form.Control as="select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </Form.Control>
+                <Form.Text>Choose an application</Form.Text>
+            </Form.Group>
+            <Modal.Footer>
+                <Button onClick={handleSubmit()}>Submit</Button>
+                <Button onClick={props.onClick}>Close</Button>
+            </Modal.Footer>
+        </Form>
     );
+}
+
+function handleSubmit() {
+    console.log("test");
+    axios.get('http://localhost/keys/?user=27a53581-f09a-4a07-97a2-3454342f7ccb')
+        .then(res => {
+            console.log(res.data);
+        });
 }
 
 export default RequestForm;
