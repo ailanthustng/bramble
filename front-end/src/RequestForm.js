849bd5222e4be703c9af0b43889362e9c2146e62
ailanthustng <ailanthustng@gmail.com>
2020-05-08 19:01:18 +0800

    Handle submit of request form & creating api key with api call

--- front-end/src/RequestForm.js
+++ front-end/src/RequestForm.js
@@ -1,40 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 import Form from "react-bootstrap/Form";
 
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Alert from "react-bootstrap/Alert";
 
 function RequestForm(props) {
+  const [app, setApp] = useState(null);
+  const [show, setShow] = useState(false);
 
-    return (
-        <Form>
-            <Form.Group controlId="Application">
-                <Form.Label>Application Name</Form.Label>
-                <Form.Control as="select">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </Form.Control>
-                <Form.Text>Choose an application</Form.Text>
-            </Form.Group>
-            <Modal.Footer>
-                <Button onClick={handleSubmit()}>Submit</Button>
-                <Button onClick={props.onClick}>Close</Button>
-            </Modal.Footer>
-        </Form>
-    );
-}
+  const handleChange = (e) => {
+    setApp(e.target.value);
+  }
 
-function handleSubmit() {
-    console.log("test");
-    axios.get('/api/keys/?user=27a53581-f09a-4a07-97a2-3454342f7ccb')
+  function handleSubmit() {
+    if (!app) {
+      setShow(true);
+    } else {
+      setShow(false);
+      let user = '27a53581-f09a-4a07-97a2-3454342f7ccb';
+
+      axios.post('/api/keys/', null, {
+        params: {
+          user, app
+        }
+      })
         .then(res => {
-            console.log(res.data);
+          console.log(res.status);
+          console.log(res.data);
         });
+      props.onClick();
+    }
+  }
+
+  return (
+    <Form>
+      <Form.Group controlId="Application">
+        <Form.Label>Application Name</Form.Label>
+        <Form.Control as="select" onChange={handleChange}>
+          <option>Please choose an application.</option>
+          <option value="foo">foo</option>
+          <option value="bar">bar</option>
+          <option value="baz">baz</option>
+        </Form.Control>
+        <Form.Text>Choose an application</Form.Text>
+      </Form.Group>
+      <Alert show={show} variant="danger" onClose={() => setShow(false)}>Please choose an application!</Alert>
+      <Modal.Footer>
+        <Button onClick={() => handleSubmit(props)}>Submit</Button>
+        <Button onClick={props.onClick}>Close</Button>
+      </Modal.Footer>
+    </Form>
+  );
 }
 
 export default RequestForm;
